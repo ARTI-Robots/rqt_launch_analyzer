e02dc930402e7d5bc33d732037e1a035b9758841
Alexander Buchegger <alexander.buchegger@student.tugraz.at>
2015-07-10 19:03:35 +0200

    rqt_launch_analyzer: implemented tree model and substitution args

--- rqt/rqt_launch_analyzer/src/rqt_launch_analyzer/analysis.py
+++ rqt/rqt_launch_analyzer/src/rqt_launch_analyzer/analysis.py
@@ -12,7 +12,7 @@ class LaunchFileParser(object):
 
     def __init__(self):
         self._rospack = rospkg.RosPack()
-        
+
     def parse(self, package_name, launch_file_name):
         launch_file_path = os.path.join(self._rospack.get_path(package_name), 'launch', launch_file_name)
         root_sc = SubstitutionContext()
@@ -34,22 +34,50 @@ class LaunchFileParser(object):
         e = LaunchElement(parent, xml_elem.tag)
         for name, value in xml_elem.items():
             a = LaunchAttribute(name, value)
-            self._evaluate(a, sc)
+            self._evaluate_attribute(a, sc)
             e.attributes.append(a)
+            if a.name in ('if', 'unless'):
+                if a.evaluated_value in ('true', 1):
+                    e.enabled = a.name == 'if'
+                elif a.evaluated_value in ('false', 0):
+                    e.enabled = a.name == 'unless'
+                else:
+                    e.enabled = None # invalid
+
         child_sc = SubstitutionContext(sc)
         for xml_child_elem in xml_elem:
             child = self._parse_element(xml_child_elem, e, child_sc)
             e.children.append(child)
         return e
 
-    def _evaluate(self, attribute, sc):
+    def _evaluate_attribute(self, attribute, sc):
         if LaunchFileParser._ATTRIBUTE_CRE.match(attribute.raw_value) is None:
             rospy.warn('Attribute %s does not match RE' % attribute.raw_value)
         else:
-            evaluated_value = ''
+            attribute.evaluated_value = ''
             for match in LaunchFileParser._SUBST_ARG_CRE.finditer(attribute.raw_value):
-                evaluated_value += '<%s>' % match.group(0)
-            attribute.evaluated_value = evaluated_value
+                sa = self._evaluate_match(match, sc)
+                attribute.parts.append(sa)
+                if sa.evaluated_value is None:
+                    attribute.evaluated_value = None
+                elif attribute.evaluated_value is not None:
+                    attribute.evaluated_value += sa.evaluated_value
+
+    def _evaluate_match(self, match, sc):
+        if match.group(1): # plain text
+            sa = SubstitutionArg(None, None, match.group(0), match.group(0))
+        else:
+            sa = SubstitutionArg(match.group(2), match.group(3), match.group(0), None)
+            if sa.tag == 'env':
+                sa.evaluated_value = sc.get_env(sa.key)
+            elif sa.tag == 'optenv':
+                value = sc.get_env(sa.key)
+                sa.evaluated_value = value if value is not None else match.group(4)
+            elif sa.tag == 'anon':
+                sa.evaluated_value = sc.get_anon(sa.key)
+            elif sa.tag == 'arg':
+                sa.evaluated_value = sc.get_arg(match.group(3))
+        return sa
 
 
 class LaunchElement(object):
@@ -57,7 +85,7 @@ class LaunchElement(object):
         self.parent = parent
         self.children = []
         self.tag = tag
-        self.enabled = None
+        self.enabled = True
         self.attributes = []
         self.exists = None
 
@@ -71,12 +99,11 @@ class LaunchAttribute(object):
 
 
 class SubstitutionArg(object):
-    def __init__(self, tag, key, raw_value, default_value=None):
+    def __init__(self, tag, key, raw_value, evaluated_value):
         self.tag = tag
         self.key = key
         self.raw_value = raw_value
-        self.evaluated_value = None
-        self.default_value = default_value
+        self.evaluated_value = evaluated_value
 
 
 class SubstitutionContext(object):
@@ -85,8 +112,8 @@ class SubstitutionContext(object):
         self._anon = {}
         self._arg = {}
         self.parent = parent
-        
-    def get_env(key):
+
+    def get_env(self, key):
         if key in self._env:
             value = self._env[key]
             value[1] += 1
@@ -97,7 +124,7 @@ class SubstitutionContext(object):
             self._env[key] = [None, 1]
             return None
 
-    def get_anon(key):
+    def get_anon(self, key):
         if key in self._anon:
             value = self._anon[key]
             value[1] += 1
@@ -107,7 +134,7 @@ class SubstitutionContext(object):
             self._anon[key] = [result, 1]
             return result
 
-    def get_arg(key):
+    def get_arg(self, key):
         if key in self._arg:
             value = self._arg[key]
             value[1] += 1
--- rqt/rqt_launch_analyzer/src/rqt_launch_analyzer/gui.py
+++ rqt/rqt_launch_analyzer/src/rqt_launch_analyzer/gui.py
@@ -4,6 +4,7 @@ import rospkg
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
+from python_qt_binding.QtCore import (QAbstractItemModel, QModelIndex, Qt)
 from python_qt_binding.QtGui import (QWidget, QIcon)
 
 from .analysis import LaunchFileParser
@@ -16,14 +17,13 @@ class Gui(Plugin):
     def __init__(self, context):
         super(Gui, self).__init__(context)
         self.setObjectName(PACKAGE_NAME)
-        
+
         self._widget = QWidget()
         ui_file = os.path.join(rospkg.RosPack().get_path(PACKAGE_NAME), 'resource', 'launch_analyzer.ui')
         loadUi(ui_file, self._widget)
-        
+
         self._widget.refresh_button.setIcon(QIcon.fromTheme('view-refresh'))
         self._widget.refresh_button.pressed.connect(self._refresh)
-        
         context.add_widget(self._widget)
 
     def save_settings(self, plugin_settings, instance_settings):
@@ -37,8 +37,9 @@ class Gui(Plugin):
     def _refresh(self):
         parser = LaunchFileParser()
         root_elem, sc = parser.parse(self._widget.package_edit.text(), self._widget.launch_file_edit.text())
-        self._dump_elem(root_elem, '')
-        
+        #self._dump_elem(root_elem, '')
+        self._widget.launch_file_tree_view.setModel(LaunchTreeModel(root_elem))
+
     def _dump_elem(self, elem, indentation):
         attrs = ''
         for attr in elem.attributes:
@@ -48,3 +49,91 @@ class Gui(Plugin):
             self._dump_elem(child, indentation + '  ')
 
 
+class LaunchTreeItemState:
+    OK = 0
+    ERROR = 1
+    CHILD_ERROR = 2
+    DISABLED = 3
+
+
+class LaunchTreeModel(QAbstractItemModel):
+    _ICONS = {
+        'DEFAULT': 'help-contents',
+        'launch': 'media-playback-start',
+        'node': 'application-x-executable',
+        'machine': 'computer',
+        'include': 'media-playback-start',
+        'remap': 'document-properties',
+        'env': 'document-properties',
+        'param': 'document-properties',
+        'rosparam': 'document-properties',
+        'group': 'folder',
+        'test': 'application-x-executable',
+        'arg': 'insert-text'
+    }
+
+    _icon_cache = {}
+
+    def __init__(self, root_elem):
+        super(QAbstractItemModel, self).__init__()
+        self.root_elem = root_elem
+        self._build()
+
+    def index(self, row, column, parent):
+        p = parent.internalPointer() if parent.isValid() else self._root_item
+        if row >= 0 and row < len(p.children) and column == 0:
+            return self.createIndex(row, column, p.children[row])
+        return QModelIndex()
+
+    def parent(self, index):
+        if index.isValid():
+            item = index.internalPointer()
+            if item.parent is not self._root_item:
+                return self.createIndex(item.parent.row, 0, item.parent)
+        return QModelIndex()
+
+    def rowCount(self, parent):
+        p = parent.internalPointer() if parent.isValid() else self._root_item
+        return len(p.children)
+
+    def columnCount(self, parent):
+        return 1
+
+    def data(self, index, role):
+        if index.isValid():
+            item = index.internalPointer()
+            if role == Qt.DisplayRole:
+                return item.title
+            elif role == Qt.DecorationRole:
+                if item.icon not in self._icon_cache:
+                    self._icon_cache[item.icon] = QIcon.fromTheme(item.icon)
+                return self._icon_cache[item.icon]
+        return None
+
+    def _build(self):
+        self._root_item = LaunchTreeItem(None, None, None, None, None)
+        self._add_child(self._root_item, self.root_elem)
+
+    def _add_child(self, item, elem):
+        icon = LaunchTreeModel._ICONS[elem.tag if elem.tag in LaunchTreeModel._ICONS else 'DEFAULT']
+        state = LaunchTreeItemState.OK if elem.enabled else LaunchTreeItemState.DISABLED
+        for a in elem.attributes:
+            if a.evaluated_value is None:
+                state = LaunchTreeItemState.ERROR
+
+        child = LaunchTreeItem(item, len(item.children), elem.tag, icon, state)
+        item.children.append(child)
+        for child_elem in elem.children:
+            child_child = self._add_child(child, child_elem)
+            if child_child.state in (LaunchTreeItemState.ERROR, LaunchTreeItemState.CHILD_ERROR):
+                child.state = LaunchTreeItemState.CHILD_ERROR
+        return child
+
+class LaunchTreeItem(object):
+    def __init__(self, parent, row, title, icon, state):
+        self.parent = parent
+        self.row = row
+        self.title = title
+        self.icon = icon
+        self.state = state
+        self.children = []
